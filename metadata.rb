86f68f0580852e406766e9d63c4b6f30c473f002
Edmund Haselwanter <me@ehaselwanter.com>
2014-02-28 12:42:54 +0100

    update license

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@ name             'iteh-gitlab'
 maintainer       'Edmund Haselwanter'
 maintainer_email 'me@ehaselwanter.com'
 license          'All rights reserved'
+license          'Apache2'
 description      'Installs/Configures gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
