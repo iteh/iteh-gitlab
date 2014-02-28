c452b8fc8bfb99b8b643b10b0d8a7327b6d4ff11
Edmund Haselwanter <me@ehaselwanter.com>
2014-02-28 12:43:34 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name             'iteh-gitlab'
 maintainer       'Edmund Haselwanter'
 maintainer_email 'me@ehaselwanter.com'
-license          'All rights reserved'
 license          'Apache2'
 description      'Installs/Configures gitlab'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu'
 
